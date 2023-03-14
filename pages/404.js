c4b2622e9e41dff16a0edc943f9cf0812951c2ff
aashahin <almhtrfahmed123@gmail.com>
2023-03-14 14:34:05 +0200

    Tutorial Done!

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+
+export default function Custom404() {
+    return <>
+    <Head>
+        <title>Not Found</title>
+    </Head>
+    <h1 style={{textAlign:"center"}}>404 - Page Not Found</h1>
+    </>;
+  }
